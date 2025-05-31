47a1050915ee8570a8e33276801f1c15cb303581
Pradeep <pradeepverma1997@gmail.com>
2025-05-31 16:05:13 +0530

    updating surefire plugin

--- src/test/java/TestRunner/Web/RunnerFile.java
+++ src/test/java/TestRunner/Web/RunnerFile.java
@@ -8,17 +8,19 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //import org.junit.runner.RunWith;
-
 //@RunWith(Cucumber.class)
+
 @CucumberOptions( features = "src/test/java/resources/Web/features",
 					glue= "stepDefinitions",
-					//tags = "@registeruser or @existinguser",
+					//tags = "@login",		//"@registeruser or @existinguser",
 					plugin = {	"pretty",
 								"html:target/cucumber-reports/cucumber-html-report.html"},
 		 					// { "pretty", "json:target/cucumber-reports/Cucumber.json" },
 							//	"junit:target/cucumber-reports/Cucumber.xml"
 					monochrome = true
 					//dryRun = true
+
+		// mvn clean test -Dcucumber.options = "--tags @login"
 				)
 
 public class RunnerFile extends AbstractTestNGCucumberTests
